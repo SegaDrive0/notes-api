8d41ac35d3f94b859f2e1970d2f7fcd4a0a7c329
SegaDrive0 <vany.orlov2016@yandex.ru>
2025-12-16 14:07:28 +0300

    Delete default module & create GET \notes

--- src/notes/notes.service.ts
+++ src/notes/notes.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
 
+export interface Note {
+  id: number;
+  title: string;
+  content: string;
+}
+
 @Injectable()
-export class NotesService {}
+export class NotesService {
+  private notes: Note[] = [
+    { id: 1, title: 'First title', content: 'My first content' },
+  ];
+
+  getNotes(): Note[] {
+    return this.notes;
+  }
+}
