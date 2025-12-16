3e0446dece30a9d4b3840e466c92ef0cc8043ca5
SegaDrive0 <vany.orlov2016@yandex.ru>
2025-12-16 15:30:18 +0300

    CRUD note service & controllers

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { NotesService } from './notes.service';
 
 @Controller('notes')
@@ -8,5 +16,26 @@ export class NotesController {
   @Get()
   getNotes() {
     return this.noteService.getNotes();
+  }
+
+  @Post()
+  createNote(@Body() body: { title: string; content: string }) {
+    return this.noteService.createNote(body.title, body.content);
+  }
+
+  @Put(':id')
+  editNote(
+    @Param('id') id: string,
+    @Body() body: { title: string; content: string },
+  ) {
+    let numberId = Number(id.slice(1, id.length));
+    return this.noteService.editNote(numberId, body.title, body.content);
+  }
+
+  @Delete(':id')
+  deleteNote(@Param('id') id: string) {
+    let numberId = Number(id.slice(1, id.length));
+
+    return this.noteService.deleteNote(numberId);
   }
 }
--- src/notes/notes.service.ts
+++ src/notes/notes.service.ts
@@ -6,6 +6,10 @@ export interface Note {
   content: string;
 }
 
+export type ServiceResult<T> =
+  | { success: true; data: T }
+  | { success: false; reason: 'NOT_FOUND' };
+
 @Injectable()
 export class NotesService {
   private notes: Note[] = [
@@ -14,5 +18,48 @@ export class NotesService {
 
   getNotes(): Note[] {
     return this.notes;
+  }
+
+  createNote(title: string, content: string): Note {
+    const newNote = {
+      id: Date.now(),
+      title,
+      content,
+    };
+
+    this.notes.push(newNote);
+
+    return newNote;
+  }
+
+  editNote(id: number, title: string, content: string): ServiceResult<Note> {
+    let note = this.notes.find((note) => note.id === id);
+
+    if (!note) {
+      return { success: false, reason: 'NOT_FOUND' };
+    }
+
+    note.title = title;
+    note.content = content;
+
+    return { success: true, data: note };
+  }
+
+  deleteNote(id: number): ServiceResult<Note> {
+    let index = this.notes.findIndex((note) => note.id === id);
+
+    if (index === -1) {
+      return { success: false, reason: 'NOT_FOUND' };
+    }
+
+    let deletedNote = this.notes[index];
+
+    if (!deletedNote) {
+      return { success: false, reason: 'NOT_FOUND' };
+    }
+
+    this.notes.splice(index, 1);
+
+    return { success: true, data: deletedNote };
   }
 }
