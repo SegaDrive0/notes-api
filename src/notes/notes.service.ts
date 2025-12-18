6f59db729667a8d68c4f01abcf01ba4f3a084446
SegaDrive0 <vany.orlov2016@yandex.ru>
2025-12-18 11:40:42 +0300

    Error Handling 404 'Not Found'

--- src/notes/notes.service.ts
+++ src/notes/notes.service.ts
@@ -1,41 +1,65 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Note } from './notes.types';
 import { PrismaService } from 'prisma/prisma.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class NotesService {
   constructor(private readonly prisma: PrismaService) {}
 
   async getNotes(): Promise<Note[]> {
-    return this.prisma.note.findMany();
+    return await this.prisma.note.findMany();
   }
 
-  async getNoteById(id: number): Promise<Note | null> {
-    return this.prisma.note.findUnique({
+  async getNoteById(id: number) {
+    const note = await this.prisma.note.findUnique({
       where: { id },
     });
+
+    if (!note) {
+      throw new NotFoundException(`Note with id: ${id} not found`);
+    }
+
+    return note;
   }
 
-  async createNote(title: string, content: string): Promise<Note> {
-    return this.prisma.note.create({
+  async createNote(title: string, content: string) {
+    return await this.prisma.note.create({
       data: { title, content },
     });
   }
 
-  async updateNote(
-    id: number,
-    title: string,
-    content: string,
-  ): Promise<Note | null> {
-    return this.prisma.note.update({
-      where: { id },
-      data: { title, content },
-    });
+  async updateNote(id: number, title: string, content: string) {
+    try {
+      return await this.prisma.note.update({
+        where: { id },
+        data: { title, content },
+      });
+    } catch (e) {
+      if (
+        e instanceof Prisma.PrismaClientKnownRequestError &&
+        e.code === 'P2025'
+      ) {
+        throw new NotFoundException(`Note with id: ${id} not found`);
+      }
+      throw e;
+    }
   }
 
-  async deleteNote(id: number): Promise<Note> {
-    return this.prisma.note.delete({
-      where: { id },
-    });
+  async deleteNote(id: number) {
+    try {
+      return await this.prisma.note.delete({
+        where: { id },
+      });
+    } catch (e) {
+      console.log(e);
+      if (
+        e instanceof Prisma.PrismaClientKnownRequestError &&
+        e.code === 'P2025'
+      ) {
+        throw new NotFoundException(`Note with id: ${id} not found`);
+      }
+      throw e;
+    }
   }
 }
