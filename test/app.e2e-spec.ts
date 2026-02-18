b4ae0d30281d58675c570024c958708c6c3dbe99
SegaDrive0 <vany.orlov2016@yandex.ru>
2026-02-18 10:48:57 +0300

    add alias to e2e config & create e2e test scenario

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -6,6 +6,7 @@ import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
+  let createdNoteId: number;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -16,10 +17,40 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('POST /notes should create note', async () => {
+    const res = await request(app.getHttpServer())
+      .post('/notes')
+      .send({
+        title: 'Test',
+        content: 'Test content',
+      })
+      .expect(201);
+
+    createdNoteId = res.body.id;
+  });
+
+  it('GET /notes/:id should get info note', async () => {
+    await request(app.getHttpServer())
+      .get('/notes/' + createdNoteId)
+      .expect(200);
+  });
+
+  it('PUT /notes/:id should update note', async () => {
+    await request(app.getHttpServer())
+      .put('/notes/' + createdNoteId)
+      .send({ title: 'E2e Test', content: 'E2e Test content' })
+      .expect(200);
+  });
+
+  it('DELETE /notes/:id should delete note', async () => {
+    await request(app.getHttpServer())
+      .delete('/notes/' + createdNoteId)
+      .expect(200);
+  });
+
+  it('GET deleted note should return 404', async () => {
+    await request(app.getHttpServer())
+      .get('/notes/' + createdNoteId)
+      .expect(404);
   });
 });
